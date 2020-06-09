b69ae400c2e625637814195d674bd5dbe9ac32f7
Shruthi S Nair <shruthi.snair91@gmail.com>
2020-06-10 00:13:52 +0530

    alert box enabled

--- js/todolist.js
+++ js/todolist.js
@@ -1,7 +1,6 @@
 var xhttp=new XMLHttpRequest();
 var f=0;
 xhttp.onreadystatechange=function()
-
 {
     if(xhttp.readyState==4 && xhttp.status==200)
     
@@ -14,16 +13,19 @@ xhttp.onreadystatechange=function()
                     // });
                 
         var todo = document.getElementById("todolist");
+        var count = 0;
         for(var i=0;i<listObj.length;i++)
         {
             var chk = document.createElement("input");
             chk.type = 'checkbox';
+            //chk.name='chkbox';
             chk.id = listObj[i].id;
             if (listObj[i].completed)
             {
                 chk.checked = true;
                 chk.disabled = true;
-            }         
+            }
+                     
             // chk.onclick = "checkClicked()";
             var label = document.createElement('label');
             label.appendChild(document.createTextNode(listObj[i].title));
@@ -33,20 +35,24 @@ xhttp.onreadystatechange=function()
             todo.appendChild(lbr);
             hr=document.createElement("hr");
             todo.appendChild(hr);
-              
-        } 
-        for(let i=0;i<listObj.length;i++)
-        {
-            const checkboxes = document.querySelectorAll(`input[chk.id="${listObj[i].id}"]:checked`);
-            // document.getElementById(chk.id).addEventListener('change', function(c){
-                checkboxes.forEach((checkbox) => {
-                    alert("checked");
-                });
-              ;
-               
+            chk.addEventListener('change', function() {
+                if (this.checked)
+                {
+                    count  +=1;
+                    Promise.resolve('Success').then(function(value) {
+                        if (count == 5)
+                        {
+                            alert("Congratulations!! You have completed 5 Tasks")
+                            count = 0;
+                        }
+                      }, function(value) {
+                        // not called
+                      });
+                }
+
+            });
+            
         }
-       
-        
     }
 }
 xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
